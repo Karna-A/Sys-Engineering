5f389041cd5350fe3a98257e8538b5ba2ea9c5ef
Karna Amit <amitaalok34@gmail.com>
2020-02-26 22:28:31 +1100

    Same code as last commit

--- Arduino Stuff/Touch Wifi/sketch.ino
+++ Arduino Stuff/Touch Wifi/sketch.ino
@@ -10,7 +10,7 @@ bool ButtonState = false;
 void setup()
 {
   Serial.begin(115200);
-  delay(10); // give me time to bring up serial monitor
+  delay(100); // give me time to bring up serial monitor
 
 
   Serial.println();
@@ -35,8 +35,8 @@ int value = 0;
 void loop()
 {
 
-  //if (touchRead(T0)==0) Serial.println("Button Pressed"); // Reads the touch pin 0 and if the pin is touched ie connected to ground it value will be 0 
+  // if (touchRead(T0)==0) Serial.println("Button Pressed"); // Reads the touch pin 0 and if the pin is touched ie connected to ground it value will be 0 
   //    delay(1000);
-  //if (touchRead(T0)>0) Serial.println("Button not Pressed");
-  //    delay(1000);
+  // if (touchRead(T0)>0) Serial.println("Button not Pressed");
+  //     delay(1000);
 }
